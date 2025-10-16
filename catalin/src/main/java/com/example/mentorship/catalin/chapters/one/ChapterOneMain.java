64e2f87f041870d4d51e2ac5d330b839f7b2f833
LonnDevelopment <lonn.development@gmail.com>
2025-10-16 08:44:38 +0300

    Create modules, set up maven

---
+++ catalin/src/main/java/com/example/mentorship/catalin/chapters/one/ChapterOneMain.java
@@ -0,0 +1,8 @@
+package com.example.mentorship.catalin.chapters.one;
+
+public class ChapterOneMain {
+
+    public static void main(String[] args) {
+        System.out.println("Chapter One code");
+    }
+}
