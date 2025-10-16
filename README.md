a148624b4b08a5554996d842ae8ea07ca584015e
MihaiCatalin97 <mihai.catalin197@gmail.com>
2025-10-16 08:45:17 +0300

    Initial setup (#2)
    Co-authored-by: LonnDevelopment <lonn.development@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mentorship-training-2025
+
+## Description
+
+This repository is to be used during the 2025 Mentorship training programme.
+
+Each mentee has a folder into which he or she would merge his changes.
+
+Direct pushes to the main branch are prohibited. 
+All changes must go through a Pull Request.
+Pull Requests require at least an approval before merging.
---
+++ catalin/src/main/java/com/example/mentorship/catalin/assesment/AssessmentMain.java
@@ -0,0 +1,32 @@
+package com.example.mentorship.catalin.assesment;
+
+public class AssessmentMain {
+
+    public static void main(String[] args) {
+        firstQuestion();
+        secondQuestion();
+    }
+
+    private static void firstQuestion() {
+        final int score1 = 8, score2 = 3;
+        Integer myScore = 7;
+        var goal = switch (myScore) {
+            case score1, score2, 7 -> "good";
+            case Integer i when i < 10 -> "better";
+            case Integer i when i >= 10 -> "best";
+            default -> {
+                yield "unknown";
+            }
+//            case null -> "nope";
+        };
+        System.out.print(goal);
+    }
+
+    private static void secondQuestion() {
+        int moon = 9, star = 2 + 2 * 3;
+        float sun = star > 10 ? 1 : 3;
+        double jupiter = (sun + moon) - 1.0f;
+        int mars = --moon <= 8 ? 2 : 3;
+        System.out.println(sun + ", " + jupiter + ", " + mars);
+    }
+}
